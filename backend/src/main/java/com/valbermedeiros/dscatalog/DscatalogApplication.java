ffb7650d02cff38be80f80c8a6bf81d82f9107cf
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-05-19 19:20:00 -0300

    Project created

---
+++ backend/src/main/java/com/valbermedeiros/dscatalog/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.valbermedeiros.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
