29366243ccafb69dab68f71b8e0d8db88eef22d8
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-05-19 21:57:29 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/valbermedeiros/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/valbermedeiros/dscatalog/resources/CategoryResource.java
@@ -4,6 +4,7 @@ import com.valbermedeiros.dscatalog.dto.CategoryDto;
 import com.valbermedeiros.dscatalog.services.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,12 @@ public class CategoryResource {
     public ResponseEntity<List<CategoryDto>> findAll() {
         List<CategoryDto> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDto> findById(@PathVariable Long id) {
+        CategoryDto dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 
 }
--- backend/src/main/java/com/valbermedeiros/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/valbermedeiros/dscatalog/services/CategoryService.java
@@ -3,15 +3,17 @@ package com.valbermedeiros.dscatalog.services;
 import com.valbermedeiros.dscatalog.dto.CategoryDto;
 import com.valbermedeiros.dscatalog.entities.Category;
 import com.valbermedeiros.dscatalog.repositories.CategoryRepository;
+import com.valbermedeiros.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
 
-    private CategoryRepository repository;
+    private final CategoryRepository repository;
 
     public CategoryService(CategoryRepository repository) {
         this.repository = repository;
@@ -24,4 +26,10 @@ public class CategoryService {
                 .collect(Collectors.toList());
     }
 
+    public CategoryDto findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.map(CategoryDto::new).orElseThrow(() -> {
+            throw new EntityNotFoundException("Entity not found.");
+        });
+    }
 }
