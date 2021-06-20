12494c02690211d74f8062a28309451b3d6e9ee8
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-06-20 19:24:47 -0300

    Implementing oauth2 authorization server.

---
+++ backend/src/main/java/com/valbermedeiros/dscatalog/config/WebSecutiryConfig.java
@@ -0,0 +1,42 @@
+package com.valbermedeiros.dscatalog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecutiryConfig extends WebSecurityConfigurerAdapter {
+
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+
+    public WebSecutiryConfig(BCryptPasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/actuator/**");
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+}
--- backend/src/main/java/com/valbermedeiros/dscatalog/services/UserService.java
+++ backend/src/main/java/com/valbermedeiros/dscatalog/services/UserService.java
@@ -9,10 +9,15 @@ import com.valbermedeiros.dscatalog.repositories.RoleRepository;
 import com.valbermedeiros.dscatalog.repositories.UserRepository;
 import com.valbermedeiros.dscatalog.services.exceptions.DataBaseException;
 import com.valbermedeiros.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,8 +26,9 @@ import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
     private final UserRepository repository;
     private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder passwordEncoder;
@@ -89,5 +95,16 @@ public class UserService {
             var role = roleRepository.getOne(roleDto.getId());
             entity.getRoles().add(role);
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = repository.findByEmail(username);
+        if(user == null) {
+            logger.error("User not found: {}", username);
+            throw new UsernameNotFoundException("EMail not found");
+        }
+        logger.info("User found: {}", username);
+        return user;
     }
 }
