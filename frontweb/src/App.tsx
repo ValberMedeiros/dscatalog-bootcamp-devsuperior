fc5b6cfc3d38958fc17b2de6b5889e561c7b43dd
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-07-14 23:17:52 -0300

    Navbar estilizado

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,13 @@
-import './assets/styles/custom.scss'
+import NavBar from './components/Navbar';
+import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello DSCatalog</h1>
+    <>
+      <NavBar />
+      <h1>Hello DSCatalog</h1>
+    </>
   );
 }
 
