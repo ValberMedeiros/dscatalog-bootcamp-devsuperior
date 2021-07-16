5ade03341cfc0adf5c87cd28a9f9203011f0b648
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-07-16 20:30:49 -0300

    Botão estilizado e adicionado Props

--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -1,12 +1,22 @@
-import './styles.css'
+import './styles.css';
+import { ReactComponent as ArrowIcon } from 'assets/images/Seta.svg';
 
-const ButtonIcon = () => {
+type Props = {
+    text: string;
+}
 
-    return (
-        <button className="btn btn-primary btn-icon">
-            <h6>INICIE AGORA A SUA BUSCA</h6>
-        </button>
-    );
+const ButtonIcon = ({ text } : Props) => {
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>{ text }</h6>
+      </button>
+
+      <div className="btn-icon-container">
+        <ArrowIcon />
+      </div>
+    </div>
+  );
 };
 
 export default ButtonIcon;
